6c06f1bb606ca7389b37f2d0bcd042b9e1890fc1
gwarz <gmwarzecha@tutanota.com>
2021-02-26 15:35:41 -0700

    added middleware and HTML routes

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const fs = require('fs')
+//const path = req('path');
+const fs = require('fs');
 
-
+// instantiantes express server
 const app = express();
-const port = process.env.port || 3003;
+const PORT = process.env.port || 3003;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//  middleware that specifies root directory serving static assets
+app.use(express.static('public'));
+
+//HTML routes
 app.get('/', (req, res) => {
-  res.send('Working');
-}
-)
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-app.listen(port, () => {
-  console.log(`Server now on port ${port}`);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+
+
+
+// listen method for server
+app.listen(PORT, () => {
+  console.log(`Server now on port ${PORT}`);
 })
